e0e9ab21c3157bde132a8c042555dc5a2b3ddb40
SOUFIANE Ousseini <s.ousseini@peris-solutions.com>
2025-04-07 21:53:30 +0000

    initial

---
+++ src/main/java/com/transi/flex/TransiFlexApplication.java
@@ -0,0 +1,13 @@
+package com.transi.flex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransiFlexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransiFlexApplication.class, args);
+    }
+
+}
